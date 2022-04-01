949d1104f9c49c8dba90bf3e3cf1b9de18aca9b8
mihairz <razvanibrian@gmail.com>
2022-04-01 03:02:09 +0300

    cleaner app.js + import express-fileupload

--- app.js
+++ app.js
@@ -1,79 +1,48 @@
-// PART 1
-    const express = require('express');
+const express = require('express');
+const app = express();
 
-    const app = express();
+const dotenv = require('dotenv'); 
+dotenv.config({path:'./.env'});
 
-    //app.get("/", (req,res) => {
-    //    //res.send("<h1>Homepage</h1>")
-    //    res.render("index")
-    //})
+const path = require('path');
+const publicDirectory = path.join(__dirname,'./public');
+app.set('view engine','hbs');
+app.use(express.static(publicDirectory));
 
-    const cookieParser = require('cookie-parser');//PART7
+const cookieParser = require('cookie-parser');
+app.use(express.urlencoded({ extended: false})); // Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.json()); // Parse JSON bodies (as sent by API clients)
+app.use(cookieParser());
 
-    // Parse URL-encoded bodies (as sent by HTML forms) PART5
-    app.use(express.urlencoded({ extended: false}));
-    // Parse JSON bodies (as sent by API clients) PART5
-    app.use(express.json()); 
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 
-    app.use(cookieParser());//PART7 
-    
-    //Define Routes
-    app.use('/', require('./routes/pages')); //PART5
-    app.use('/auth', require('./routes/auth')); //PART5
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+const mysql = require('mysql'); 
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
 
-    app.listen(x=5000, ()=> {
-        console.log("Server started on port "+x);
-    })
-
-
-// PART 2
-    const dotenv = require('dotenv'); //PART3
-
-    dotenv.config({path:'./.env'}); //PART3
-
-    const mysql = require('mysql'); 
-
-    const db = mysql.createConnection({
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE
-    })
-
-    db.connect((error)=>{
-        if(error){
-            console.log(error);
-        } else{
-            console.log("MYSQL CONNECTED");
-        }
-    })
+db.connect((error)=>{
+    if(error){
+        console.log(error);
+    } else{
+        console.log("MYSQL CONNECTED");
+    }
+})
 
 
-//PART 3
-    app.set('view engine','hbs');
-    
-    const path = require('path');
-    const publicDirectory = path.join(__dirname, 
-    './public');
-
-    app.use(express.static(publicDirectory));
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
 
 
-//PART 4
-    //app.get("/register", (req,res) => {
-    //    res.render("register")
-    //})
-
-
-//PART 5
-    // Parse URL-encoded bodies (as sent by HTML forms)
-    // Parse JSON bodies (as sent by API clients)
-
-
-//PART 6
-
-
-//PART7
-        
-    
+app.listen(port=5000, ()=> {
+    console.log("Server started on port "+port);
+})
